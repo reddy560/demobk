c379528f0b105752da1bcee4ece4ed9b738c3414
mkps22 <69manishkumar@gmail.com>
2024-04-25 12:55:55 +0530

    ok1

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let express=require("express")
 let app=express()
+app.use(express.json())
 app.get("/",(req,res)=>{
     res.send("hello from server")
 })
 app.get("/user",(req,res)=>{
     res.send("hello from server on /user")
+})
+app.post("/add",(req,res)=>{
+    res.json(req.body)
 })
 
 app.listen(5002)
