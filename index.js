63a2634d83f12e04ec7b64778dfbbbe6c5ff9a82
mkps22 <69manishkumar@gmail.com>
2024-04-25 13:04:33 +0530

    ok1

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 let express=require("express")
+let mongoose=require("mongoose")
 let app=express()
 app.use(express.json())
+mongoose.connect("mongodb+srv://vyshnavi:12345@cluster0.hzwb4zt.mongodb.net/userdemo")
+let usersch=new mongoose.Schema({
+    "_id":Number,
+    "name":String
+})
+let user=mongoose.model("user",usersch)
+
 app.get("/",(req,res)=>{
     res.send("hello from server")
 })
 app.get("/user",(req,res)=>{
     res.send("hello from server on /user")
 })
-app.post("/add",(req,res)=>{
-    res.json(req.body)
+app.post("/add",async(req,res)=>{
+    let data=new user(req.body)
+    await data.save()
+    res.json({"msg":"added"})
 })
 
 app.listen(5002)
